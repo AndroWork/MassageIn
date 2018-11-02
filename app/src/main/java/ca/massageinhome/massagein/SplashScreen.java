5e38e6745832aba029f8b5ea85c665157abac822
Piyush01Bhatt <piyush01bhatt@gmail.com>
2018-11-02 20:54:22 +0530

    added navigation drawer n recycler view

--- app/src/main/java/ca/massageinhome/massagein/SplashScreen.java
+++ app/src/main/java/ca/massageinhome/massagein/SplashScreen.java
@@ -1,7 +1,10 @@
 package ca.massageinhome.massagein;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ImageView splashLogo = findViewById(R.id.splash_logo);
+        splashLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashScreen.this,LogIn.class));
+            }
+        });
     }
 }
