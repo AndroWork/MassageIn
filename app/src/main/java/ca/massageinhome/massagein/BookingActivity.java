0b0243b7d2cc9a1c0bc0b29c54a129bfff65437b
saharsh <saharshpandeybolt@gmail.com>
2018-11-05 21:36:57 +0530

    App Theme Changed

--- app/src/main/java/ca/massageinhome/massagein/BookingActivity.java
+++ app/src/main/java/ca/massageinhome/massagein/BookingActivity.java
@@ -3,6 +3,7 @@ package ca.massageinhome.massagein;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,10 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("   Massage In Home");
         Button button = findViewById(R.id.booking1_next);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
